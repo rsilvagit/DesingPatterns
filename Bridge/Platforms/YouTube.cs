76e377b0cdea591936fe9153d04d7d632b7310a1
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-02 20:47:25 -0300

    criação da plataforma facebook

---
+++ Bridge/Platforms/YouTube.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Platforms
+{
+    class YouTube : IPlatform
+    {
+        public YouTube() 
+        {
+            ConfigureRMTP();
+        }
+
+        public void AuthToken()
+        {
+            Console.WriteLine("Autorizando a ");
+        }
+
+        public void ConfigureRMTR()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
