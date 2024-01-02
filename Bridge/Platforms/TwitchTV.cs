84d14c8b863698613a59654e7a450d284ce09deb
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-02 20:50:06 -0300

    criando plataforma twichtv

---
+++ Bridge/Platforms/TwitchTV.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Platforms
+{
+    class TwitchTV : IPlatform
+    {
+        public TwitchTV()
+        {
+            ConfigureRMTP();
+            Console.WriteLine("TwichTV: Tranmissão iniciada");
+        }
+
+        public void AuthToken()
+        {
+            Console.WriteLine("TwitchTV: Autorizando a aplicação ");
+        }
+
+        public void ConfigureRMTP()
+        {
+            AuthToken();
+            Console.WriteLine("TwitchTV: configurando servidor RMTP.");
+
+        }
+    }
+}
