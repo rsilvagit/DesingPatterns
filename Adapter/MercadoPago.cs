3e194d042f9c3ed58de1bd848bc4f6a53a59b686
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-02 19:58:18 -0300

    criação classe para pagamento via mercado pago

---
+++ Adapter/MercadoPago.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class MercadoPago : IMercadoPago
+    {
+        private Token token;
+        public Token AuthToken()
+        {
+            return new Token();
+        }
+
+        public void MercadoReceivePayment()
+        {
+            token = this.AuthToken();
+            Console.WriteLine("Recebendo via Mercado Pago");
+        }
+
+        public void MercadoSendPayment()
+        {
+            token= this.AuthToken();
+            Console.WriteLine("Pagando via Mercado Pago");
+        }
+    }
+}
