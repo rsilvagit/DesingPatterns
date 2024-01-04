eefeb576b3511bf20e01ebae789aaf00e885dbd3
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-03 23:24:13 -0300

    implementando check weak password

---
+++ ChainOfResponsability/MIddlewares/CheckWeakPasswordMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsability.MIddlewares
+{
+    class CheckWeakPasswordMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (password.Equals("123456"))
+            {
+                Console.WriteLine("Senha muito fraca para sua segurança solicitamops que altere para uma senha mais forte");
+                
+            }
+            return CheckNext(email, password);
+        }
+    }
+}
--- ChainOfResponsability/Program.cs
+++ ChainOfResponsability/Program.cs
@@ -11,22 +11,23 @@ namespace ChainOfResponsability
         {
             server = new Server();
             server.RegisterUser("master@teste.com", "789456");
-            server.RegisterUser("teste2@teste.com", "741258963");
+            server.RegisterUser("teste2@teste.com", "123456");
 
             Middleware middleware = new CheckUserMiddleware(server);
             middleware.LinkWith(new CheckPermissionMiddleware());
+            middleware.LinkWith(new CheckWeakPasswordMiddleware());
             server.SetMiddleware(middleware);
         }
         static void Main(string[] args)
         {
             Init();
 
-            Boolean done;
+            Boolean done = false;
             do
             {
                 Console.WriteLine("Digite o seu e-mail");
                 string email = Console.ReadLine();
-                Console.WriteLine("Digite o sua senha");
+                Console.WriteLine("Digite o sua senha: ");
                 string passoword = Console.ReadLine();
 
                 done = server.LogIn(email, passoword);
