bd1aa22ee6bd856ddc7ae92cec056c1f6c67019d
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-03 22:34:54 -0300

    configuração das middleware na program

--- ChainOfResponsability/MIddlewares/CheckUserMiddleware.cs
+++ ChainOfResponsability/MIddlewares/CheckUserMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChainOfResponsability.Servers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace ChainOfResponsability.MIddlewares
 {
-    internal class CheckUserMiddleware
+    class CheckUserMiddleware : Middleware
     {
+        private Server server;
+        public CheckUserMiddleware(Server server )
+        {
+            this.server = server;
+        }
+        public override bool Check(string email, string password)
+        {
+            if (!server.HasEmail(email))
+            {
+                Console.WriteLine("E-mail inválido e/ou senha inválidos!");
+                return false;
+            }
+            if (!server.IsValidPassword(email, password))
+            {
+                Console.WriteLine("Senha inválida!");
+                return false;
+            }
+            return CheckNext(email, password);
+        }
     }
 }
--- ChainOfResponsability/Program.cs
+++ ChainOfResponsability/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChainOfResponsability.MIddlewares;
+using ChainOfResponsability.Servers;
+using System;
 
 namespace ChainOfResponsability
 {
     class Program
     {
+        private static Server server;
+        static void Init()
+        {
+            server = new Server();
+            server.RegisterUser("master@teste.com", "789456");
+            server.RegisterUser("teste2@teste.com", "741258963");
+
+            Middleware middleware = new CheckUserMiddleware(server);
+            middleware.LinkWith(new CheckPermissionMiddleware());
+            server.SetMiddleware(middleware);
+        }
         static void Main(string[] args)
         {
+            Init();
+
+            Boolean done;
+            do
+            {
+                Console.WriteLine("Digite o seu e-mail");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite o sua senha");
+                string passoword = Console.ReadLine();
+
+                done = server.LogIn(email, passoword);
+
+
+            } while (!done);
+            Console.ReadLine();
         }
     }
 }
