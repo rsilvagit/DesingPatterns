9842f520a61a6b9c6c173090594fd0780073aa47
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-03 22:10:13 -0300

    criando CheckPermissionMiddleware

---
+++ ChainOfResponsability/MIddlewares/CheckPermissionMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using ChainOfResponsability.Servers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsability.MIddlewares
+{
+    class CheckPermissionMiddleware : Middleware
+    {
+      
+        public override bool Check(string email, string password)
+        {
+            if (email.Equals("master@teste.com"))
+            {
+                Console.WriteLine("Seja bem-vindo administrador: ");
+                return true;
+            }
+            Console.WriteLine("Seja bem-vindo!");
+            return CheckNext(email, password);
+        }
+    }
+}
