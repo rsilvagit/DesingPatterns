d4b8fcd122cb6d7267e98d7284694407d59b0588
Rafael Silva <rafael.sdasilva84@hotmail.com>
2024-01-03 21:53:19 -0300

    criação server

---
+++ ChainOfResponsability/Servers/Server.cs
@@ -0,0 +1,43 @@
+﻿using ChainOfResponsability.MIddlewares;
+using System;
+using System.Collections.Generic;
+
+
+namespace ChainOfResponsability.Servers
+{
+   class Server
+    {
+        private Dictionary<string, string> user = new Dictionary<string, string>();
+        private Middleware middleware;
+        public void SetMiddleware(Middleware middleware)
+        {
+            this.middleware = middleware;
+        }
+
+        public Boolean LogIn(string email, string password)
+        {
+            if (middleware.Check(email,password))
+            {
+                Console.WriteLine("Usuario autenticado");
+                Console.WriteLine("Seja Bem-vindo...");
+                return true;
+            }
+            return false;
+        }
+        public void RegisterUser(string email,string password)
+        {
+            user[email] = password;
+
+        }
+        public Boolean HasEmail(string email)
+        {
+            return user.ContainsKey(email);
+        }
+        public Boolean IsValidPassword(string email, string password)
+        {
+            string value = "";
+            user.TryGetValue(email, out value);
+            return password == value;
+        }
+    }
+}
